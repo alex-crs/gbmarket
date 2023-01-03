ea3a179f1dd63298da90eaf93b1245ec6d006e6c
Andreev Aleksey <alexav5@yandex.ru>
2023-01-04 01:05:06 +0700

    bean cart add

---
+++ src/main/java/com/gb/market/market/controllers/CartController.java
@@ -0,0 +1,41 @@
+package com.gb.market.market.controllers;
+
+import com.gb.market.market.entities.Cart;
+import com.gb.market.market.services.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @GetMapping("/add/{id}")
+    public void addProductToCart(@PathVariable Long id) {
+        cartService.add(id);
+    }
+
+    @GetMapping
+    public Cart getCurrentCart() {
+        return cartService.getCurrentCart();
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteProductFromCart(@PathVariable Long id) {
+        if (id != null) {
+            cartService.delete(id);
+        }
+    }
+
+    @GetMapping("/delete/clear")
+    public void clearCart() {
+        cartService.getCurrentCart().clearCart();
+    }
+
+    @GetMapping("/delete/productAmounts/{id}")
+    public void deleteProductWithAmounts(@PathVariable Long id) {
+        cartService.getCurrentCart().deleteProductWithAmountsFromCart(id);
+    }
+
+}
