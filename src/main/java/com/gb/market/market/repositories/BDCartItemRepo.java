fdffe314013d685fe264c51cae002e99be4046a3
Andreev Aleksey <alexav5@yandex.ru>
2023-01-04 22:11:36 +0700

    revert to previous version code

--- src/main/java/com/gb/market/market/repositories/BDCartItemRepo.java
+++
@@ -1,7 +0,0 @@
-package com.gb.market.market.repositories;
-
-import com.gb.market.market.entities.BDCartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BDCartItemRepo extends JpaRepository<BDCartItem, Long> {
-}
