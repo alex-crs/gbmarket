30790ca23b01ec90a137640cafd29bdd394ae5bb
Andreev Aleksey <alexav5@yandex.ru>
2023-01-15 23:42:53 +0700

    before homework7

---
+++ cart-service/src/test/java/com/gb/market/carts/services/CartServiceTest.java
@@ -0,0 +1,22 @@
+package com.gb.market.carts.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CartServiceTest {
+
+    @Autowired
+    CartService cartService;
+
+    @Test
+    void getCurrentCart() {
+    }
+
+    @Test
+    void addToCartFromBD() {
+    }
+}
